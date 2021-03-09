23b045c8db961f409e5162a318e8eef28b45c188
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-03-09 12:24:45 +0500

    Add mouse-movement listener

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const el = document.querySelector('.box');
 let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
 
-e
+el.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    startPosX = e.clientX;
+    startPosY = e.clientY;
+
+    document.addEventListener('mousemove', mouseMove);
+
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', mouseMove)
+    })
+});
